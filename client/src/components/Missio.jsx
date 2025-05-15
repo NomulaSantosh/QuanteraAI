05cae37b64d2699c2b4b01a2520c3eaddd35a4ea
NomulaSantosh <ramunomula51@gmail.com>
2025-05-15 21:54:26 +0530

    initial commmit

--- client/src/components/Missio.jsx
+++ client/src/components/Missio.jsx
@@ -5,7 +5,7 @@ const Missio = () => {
   return (
     <div className='flex justify-center items-center my-10 gap-14'>
         <div className='flex flex-col justify-center items-center'>
-            <p className='text-5xl font-bold text-[#1363C6] py-10'>Our Mission</p>
+            <p className='text-4xl font-bold text-[#1363C6] py-10'>Our Mission</p>
             <img src={arrow} alt="" className='w-20' />
         </div>
         <p className='text-gray-500 text-2xl'>To bridge the gap between classroom learning and real-world applications.
